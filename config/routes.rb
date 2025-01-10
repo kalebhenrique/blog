3caa786946d6a22a0eaafc9b1062a16f1b1da233
kalebhenrique <kalebhenriquebr@gmail.com>
2025-01-10 20:26:13 -0300

    add routes protection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,15 +7,7 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
